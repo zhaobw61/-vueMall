4e063024a0cc61e4ebad22bcd4e6e3e6be78b33a
zhaobw <1964471757@qq.com>
2019-08-15 10:00:10 +0800

    完成商品列表组件

---
+++ src/components/NavBread.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="nav-breadcrumb-wrap">
+    <div class="container">
+      <nav class="nav-breadcrumb">
+        <a href="/">Home</a>
+        <!-- <span>Goods</span> -->
+        <slot></slot>
+      </nav>
+    </div>
+  </div>
+</template>
+<style scoped>
+</style>
+<script>
+export default {};
+</script>
---
+++ src/views/GoodsList.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <nav-bread>
+      <span>Goods</span>
+    </nav-bread>
+    <div class="accessory-result-page accessory-page">
+      <div class="container">
+        <div class="filter-nav">
+          <span class="sortby">Sort by:</span>
+          <a href="javascript:void(0)" class="default cur">Default</a>
+          <a href="javascript:void(0)" class="price">
+            Price
+            <svg class="icon icon-arrow-short">
+              <use xlink:href="#icon-arrow-short" />
+            </svg>
+          </a>
+          <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
+        </div>
+        <div class="accessory-result">
+          <!-- filter -->
+          <div class="filter stopPop" id="filter" v-bind:class="{'filterby-show':filterBy}">
+            <dl class="filter-price">
+              <dt>Price:</dt>
+              <dd>
+                <a href="javascript:void(0)" v-bind:class="{'cur':priceChecked == 'all'}" @click="priceChecked='all'">All</a>
+              </dd>
+              <dd v-for="(item,index) in priceFilter" v-bind:key="index">
+                <a href="javascript:void(0)" v-bind:class="{'cur':priceChecked == index}" @click="setPriceFilter(index)">{{item.startPrice}}-{{item.endPrice}}</a>
+              </dd>
+            </dl>
+          </div>
+
+          <!-- search result accessories list -->
+          <div class="accessory-list-wrap">
+            <div class="accessory-list col-4">
+              <ul>
+                <li v-for="(item,index) in goodList" v-bind:key="index">
+                  <div class="pic">
+                    <a href="#">
+                      <img v-lazy="'/static/'+item.productImg" alt />
+                    </a>
+                  </div>
+                  <div class="main">
+                    <div class="name">{{item.productName}}</div>
+                    <div class="price">{{item.productPrice}}</div>
+                    <div class="btn-area">
+                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+import './../assets/css/base.css';
+import './../assets/css/product.css';
+import '@/assets/css/login.css';
+import '@/assets/css/checkout.css'
+import NavHeader from '@/components/NavHeader';
+import NavFooter from '@/components/NavFooter'
+import NavBread from '@/components/NavBread';
+import axios from 'axios';
+export default {
+  name: "GoodsList",
+  data(){
+    return{
+      goodList:[],
+      priceFilter:[
+        {
+          startPrice:'0.00',
+          endPrice:'500.00'
+        },
+        {
+          startPrice:'500.00',
+          endPrice:'1000.00'
+        },
+        {
+          startPrice:'1000.00',
+          endPrice:'2000.00'
+        }
+      ],
+      priceChecked:'all',
+      filterBy:false,
+      overLayFlag:false,
+    }
+  },
+  components:{
+    NavHeader,
+    NavFooter,
+    NavBread
+  },
+  mounted:function(){
+    this.getGoodsList();
+  },
+  methods:{
+    getGoodsList(){
+      axios.get('api/goods').then((result)=>{
+        var res = result.data
+        this.goodList = res.result;
+        console.log(this.goodList);
+      });
+    },
+    showFilterPop(){
+      this.filterBy = true;
+      this.overLayFlag = true;
+    },
+    setPriceFilter(index){
+      this.priceChecked = index;
+      this.closePop();
+    },
+    closePop(){
+      this.filterBy = false;
+      this.overLayFlag = false;
+    },
+    
+  }
+};
+</script>
